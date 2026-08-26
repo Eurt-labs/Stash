0de787d005fb2346cc3b8985449046be2c94e233
Eurt-labs <dhruv15saraswat@gmail.com>
2026-08-26 17:16:43 +0530

    chore: bump application version to 2.1.0 across both platforms

--- Stash-Android/app/build.gradle.kts
+++ Stash-Android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.eurtlabs.stash"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "2.0.0"
+        versionCode = 2
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
